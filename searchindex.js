23a1120568f94e9ba07ed61e4b970d69b1e171ea
Matt O'Donnell <mbod@umich.edu>
2017-09-08 17:03:59 -0400

    first publish

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index","source/includeme"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to slicesofpie&#8217;s documentation!","&lt;no title&gt;"],titleterms:{document:0,indic:0,slicesofpi:0,tabl:0,welcom:0}})
