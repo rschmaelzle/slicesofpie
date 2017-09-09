4b8618c16655b7c27aac5020748144feb062881d
Matt O'Donnell <mbod@ASCs-MacBook-Pro.local>
2017-09-09 03:09:02 -0400

    added extensions and theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['nbsphinx', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'slicesofpie'
-copyright = '2017, mbod'
-author = 'mbod'
+copyright = '2017, mbod & rschmaelzle'
+author = 'mbod & rschmaelzle'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -102,19 +102,21 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+	'full_logo': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -283,12 +285,4 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-
-# Activate the theme.
-html_theme = 'haiku'
-html_theme_options = {
-	'full_logo': True
-
-}
 
--- docs/index.rst
+++ docs/index.rst
@@ -3,23 +3,27 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-slicesofpie
-===========
-
-Notes, examples, tutorials and ideas for computational social sicnce with a strong Pythonic flavor!
-___________________________________________________________________________________________________
-
+.. rubric::
+   Notes, examples, tutorials and ideas for computational social sicnce with a strong Pythonic flavor!
 
 Maintained by Matt O'Donnell (`mbod <https://github.com/mbod>`_) & Ralf Schmälzle (`rschmaelzle <https://github.com/rschmaelzle>`_)
 
+.. container::
+
+   slicesofpie is an evolving set of resources for use in computational social science..
+
+.. note::
+   THIS IS AN IMPORTANT NOTE
+
+.. todo::
+   add a description of scope of site
 
 
 
 Contents:
 
 .. toctree::
-   :maxdepth: 2
-
+   :maxdepth: 1
 
    includeme
 
